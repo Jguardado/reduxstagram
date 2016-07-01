d19e435ca40ae1bcbdf17ecd81fbbf3a2759bbc6
Juan Guardado <jguardado85@gmail.com>
2016-07-01 11:29:04 -0700

    completed the tutorial

--- learn-redux/client/components/main.js
+++ learn-redux/client/components/main.js
@@ -5,7 +5,7 @@ const Main = React.createClass({
   render() {
     return (
       <div>
-        <h1 Link to ="/">Reduxagrat</h1>
+        <h1><Link to ="/">Reduxagrat</Link></h1>
         {React.cloneElement(this.props.children, this.props)}
       </div>
     );
